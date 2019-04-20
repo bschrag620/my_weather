ff74aeb458da78c300c1db2eaae836d23e258d24
Brad Schrag <brad.schrag@gmail.com>
2019-04-20 12:37:36 -0500

    added namespaced api controller

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,18 @@
+class Api::UsersController < ApplicationController
+	def create
+		user = User.create(user_params)
+		if user.save
+			token = user.log_in
+			binding.pry
+			render :json => { user: user, token: token }
+		else
+			
+		end
+	end
+
+
+	private
+	def user_params
+		params.require(:user).permit(:email, :email_confirmation, :password, :password_confirmation, :first_name, :last_name, :username)
+	end
+end
