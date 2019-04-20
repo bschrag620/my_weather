9e15bec9ddf33785862b36cdcac07e75210be07d
Brad Schrag <brad.schrag@gmail.com>
2019-04-20 07:35:05 -0500

    defined active record associations between locations and users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ApplicationRecord
 	validates :email, presence: true, uniqueness: true, confirmation: true
 	validates :email_confirmation, presence: true
 
+	has_many :user_locations
+	has_many :locations, through: :user_locations
+
 	has_secure_password
 end
