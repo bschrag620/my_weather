87fa07bf60166d93523a8316e539857f07c44b51
Brad Schrag <brad.schrag@gmail.com>
2019-05-02 08:44:34 -0500

    added weatherIndex to give single location for stateless weather componnents

---
+++ client/src/components/stateless/weatherIndex.js
@@ -0,0 +1,13 @@
+import CityState from './cityState'
+import LatLng from './latLng'
+import Temperature from './temperature'
+import Wind from './wind'
+import Zip from './zip'
+
+export {
+	CityState,
+	LatLng,
+	Temperature,
+	Wind,
+	Zip
+}
