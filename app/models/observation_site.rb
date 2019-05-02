0c9b4e1306d362a9093f054cecb0b554ceada3de
Brad Schrag <brad.schrag@gmail.com>
2019-05-02 08:45:37 -0500

    added validations

--- app/models/observation_site.rb
+++ app/models/observation_site.rb
@@ -1,4 +1,7 @@
 class ObservationSite < ApplicationRecord
 	has_many :observation_site_locations
 	has_many :locations, through: :observation_site_locations
+
+	validates :code, presence: true, uniqueness: true
+	validates :name, :observation_api, :lat, :lng, presence: true
 end
