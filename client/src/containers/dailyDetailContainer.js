71c37cffc7868247f5357f5f50d888dcaffbc950
Brad Schrag <brad.schrag@gmail.com>
2019-05-02 08:46:23 -0500

    building dailyDetail

--- client/src/containers/dailyDetailContainer.js
+++ client/src/containers/dailyDetailContainer.js
@@ -1,24 +1,51 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import retrieveLocation from '../actions/location_actions'
+import retrieveCurrentConditions from '../actions/weather_actions'
+import RetreivingData from '../components/stateless/retrievingData'
+import weatherIndex from '../components/stateless/weatherIndex'
+import DetailCard from '../components/detailCard'
+
 
 class DailyDetailContainer extends Component {
 	constructor(props) {
 		super(props)
 
 		this.state = {
-			locationLoaded: false,
+			loading: true,
 			zip: this.props.match.params.zip
 		}
 	}
+
+	updateData() {
+		if (!this.props.dailyDetail) {
+ 			   console.log('no weather data')
+ 			   this.props.retrieveCurrentConditions(this.props.code)
+ 			   	.then(
+ 			   		this.setState({
+ 			   			loading: false
+ 			   		})
+ 			   	)
+		} else {
+			this.setState({
+				loading: false
+			})
+		}
+	}
+
 	componentDidMount() {
-		debugger;
+		this.props.retrieveLocation(this.state.zip)
+			.then( () => {
+				this.updateData()
+			})
 	}
 
 
 	render () {
 		return (
 			<div className="daily-detail">
-				This is a daily detail for {this.state.zip}
+				This is a daily detail for {this.state.zip} <br/>
+				{!this.props.dailyDetail ? <RetreivingData message="daily details" /> : <DetailCard details={this.props.dailyDetail} />}
 			</div>
 
 		)
@@ -26,9 +53,19 @@ class DailyDetailContainer extends Component {
 }
 
 const mapStateToProps = state => {
+	const code = state.sessionReducer.activeWeatherStationCode
 	return {
-		locations: state.locationReducer.locations
+		location: state.sessionReducer.activeLocation,
+		code: code,
+		dailyDetail: state.weatherReducer.sites[code]
 	}
 }
 
-export default connect(mapStateToProps)(DailyDetailContainer)
+const mapDispatchToProps = dispatch => {
+	return {
+		retrieveLocation: zip => dispatch(retrieveLocation(zip)),
+		retrieveCurrentConditions: (code, units) => dispatch( retrieveCurrentConditions(code, units) )
+	}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(DailyDetailContainer)
