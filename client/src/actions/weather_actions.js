565e6bfacd73ef5bd50815ad6a62cd3a36628330
Brad Schrag <brad.schrag@gmail.com>
2019-05-04 02:17:13 -0500

    added location id to payload object

--- client/src/actions/weather_actions.js
+++ client/src/actions/weather_actions.js
@@ -1,15 +1,21 @@
-export default function retrieveCurrentConditions(code, units='si') {
+export default function retrieveCurrentConditions(code, id, units='si') {
 	return dispatch => {
 		dispatch({
-			type: 'RETRIEVE_CURRENT',
-			code: code
+			type: 'RETRIEVING_CURRENT',
+			payload: {
+				loadingData: true,
+				id: id,
+				code: code
+			}
 		})
+
 		return fetch(`/api/locations/${code}/current?units=${units}`)
 				.then(resp => resp.json())
 				.then(weather => {
 					dispatch({
 						type: 'ADD_CURRENT',
-						payload: weather
+						payload: weather,
+						id: id
 					})
 				})
 	}
