0d40cf200ea7b05e9678d4d615350b3c0e0456bb
Brad Schrag <brad.schrag@gmail.com>
2019-05-04 02:49:34 -0500

    added routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
-import { Navbar } from 'react-bootstrap'
 import MyWeatherContainer from './containers/myWeatherContainer'
+import ProfileContainer from './containers/profileContainer'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
+ 	<Router>
       <div className="App">
-          <MyWeatherContainer />
+      	<Switch>
+      		<Route path='/profile' component={ProfileContainer} />
+			<Route path="/:zip([0-9]{5})?" component={MyWeatherContainer} />
+      		<Route path='/' component={MyWeatherContainer} />
+      	</Switch>
       </div>
+  	</Router>
     );
   }
 }
