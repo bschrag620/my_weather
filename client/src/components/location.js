b4d43172e1cb473fd51a3b61bd5cea8e436d7ba6
Brad Schrag <brad.schrag@gmail.com>
2019-05-04 02:19:12 -0500

    refactored

--- client/src/components/location.js
+++ client/src/components/location.js
@@ -1,67 +1,33 @@
 import React, { Component } from 'react'
 import CityState from './stateless/cityState'
 import Zip from './stateless/zip'
-import LatLng from './stateless/latLng'
-import { connect } from 'react-redux'
 import { Col } from 'react-bootstrap'
 import CompactCurrentConditionsContainer from '../containers/compactCurrentConditionsContainer'
-import retrieveCurrentConditions from '../actions/weather_actions'
 import RetrievingData from './stateless/retrievingData'
+//import { connect } from 'react-redux'
 
 
 class Location extends Component {
-	constructor(props) {
-		super(props)
 
-		this.state = {
-			loading: true
-		}
-	}
-
-	componentWillMount() {
-		if (this.props.sites[this.props.location.preferred_observation_code]) {
-			this.setState({
-					loading: false,
-					currentConditions: this.props.sites[this.props.location.preferred_observation_code].properties
-			})
-		}
-	}
 
 	componentDidMount() {
-		if (this.state.loading) {
-			const code = this.props.location.preferred_observation_code
-			this.props.retrieveCurrentConditions(code, 'si').then( () => {
-					this.setState({
-						loading: false,
-						currentConditions: this.props.sites[this.props.location.preferred_observation_code].properties
-				})
-			})
+		if (this.props.weatherSite.current.loadingData) {
+			const code = this.props.data.preferred_observation_code
+			const id = this.props.data.id
+			this.props.retrieveCurrentConditions(code, id, 'si')
 		}
 	}
-
 	
 	render() {
+
 		return (
-			<Col className="location block" id={this.props.location.id}>
-				<div><CityState city={this.props.location.city} state={this.props.location.state}/></div>
-				{this.state.loading ? <RetrievingData message="location"/> : <CompactCurrentConditionsContainer conditions={this.state.currentConditions}/>}
-				<Zip zip={this.props.location.zip} /><br/>
+			<Col xs md='auto' className="location block" id={this.props.data.id}>
+				<div><CityState city={this.props.data.city} state={this.props.data.state}/></div>
+				{this.props.weatherSite.current.loadingData ? <RetrievingData message="current conditions" /> : <CompactCurrentConditionsContainer conditions={this.props.weatherSite.current}/>}
+				<Zip zip={this.props.data.zip} /><br/>
 			</Col>
 		)
 	}
 }
 
-const mapDispatchToProps = dispatch => {
-	return {
-		retrieveCurrentConditions: 
-		(code, units) => dispatch( retrieveCurrentConditions(code, units) )
-	}
-}
-
-const mapStateToProps = state => {
-	return {
-		sites: state.weatherReducer.sites
-	}
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Location)
+export default Location
