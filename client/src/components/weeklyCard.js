813be8c7ada4b9a0d2ebb30101bb5770c7fc4620
Brad Schrag <brad.schrag@gmail.com>
2019-05-04 21:43:11 -0500

    created weekly display card

---
+++ client/src/components/weeklyCard.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+import { CityState, LatLng, Temperature, Wind, Zip } from '../components/stateless/weatherIndex'
+
+export default class WeeklyCard extends Component {
+	
+	render() {
+		return <div> hello from weekly </div>
+	}
+}
