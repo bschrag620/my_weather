1dbdd64a4ca7c56f901502e4aa8ae712f8400b53
Brad Schrag <brad.schrag@gmail.com>
2019-05-04 21:42:53 -0500

    created hourlyCard

---
+++ client/src/components/hourlyCard.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { CityState, LatLng, Temperature, Wind, Zip } from '../components/stateless/weatherIndex'
+
+export default class HourlyCard extends Component {
+	
+	render() {
+
+		return <div> hello from hourly </div>
+	}
+}
