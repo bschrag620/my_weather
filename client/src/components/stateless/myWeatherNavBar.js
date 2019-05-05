0d25e698347b9c9720c20deb2aaf71f565697d1c
Brad Schrag <brad.schrag@gmail.com>
2019-05-04 21:43:42 -0500

    created navbar for displayContainer

---
+++ client/src/components/stateless/myWeatherNavBar.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react'
+import { Navbar, NavLink } from 'react-bootstrap'
+
+export default class MyWeatherNavBar extends Component {
+	render() {
+		return <nav>this will be a navbar</nav>
+	}
+}
