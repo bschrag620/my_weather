f070680c5e4bb7d1597bfcc3458fb07c83fb2980
Brad Schrag <brad.schrag@gmail.com>
2019-05-06 07:13:07 -0500

    added action to retrieve hourly data

--- client/src/actions/weather_actions.js
+++ client/src/actions/weather_actions.js
@@ -20,3 +20,26 @@ export default function retrieveCurrentConditions(code, id, units='si') {
 				})
 	}
 }
+
+export function retrieveHourlyConditions(code, id, units='si') {
+	return dispatch => {
+		dispatch({
+			type: 'RETRIEVING_HOURLY_WEATHER',
+			payload: {
+				loadingData: true,
+				id: id,
+				code: code
+			}
+		})
+
+		return fetch(`/api/locations/${code}/hourly?units=${units}`)
+			.then(resp => resp.json())
+			.then(weather => {
+				dispatch({
+					type: 'ADD_HOURLY_WEATHER',
+					payload: weather,
+					id: id
+				})
+			})
+	}
+}
