647c312ae92051e5f3fe7c2488f938beb23560e5
Brad Schrag <brad.schrag@gmail.com>
2019-05-05 23:28:43 -0500

    build out detailCard

--- client/src/components/detailCard.js
+++ client/src/components/detailCard.js
@@ -1,27 +1,27 @@
 import React, { Component } from 'react'
 import { CityState, LatLng, Temperature, Wind, Zip } from '../components/stateless/weatherIndex'
+import RetrievingData from './stateless/retrievingData'
+
+const card = props => {
+	const properties = props.currentConditions.properties
+	const meta = props.currentConditions.meta
+
+	return (
+		<div className={meta.code}>
+			<h1>{props.currentLocation.city}, {props.currentLocation.state}</h1>
+			<h2><Temperature temperature={properties.temperature} /></h2>
+			<h3><Wind wind={properties.wind} /></h3>
+			<div className='fine-print'>Sensor location: {meta.name}</div>
+			<div className='fine-print'>Observation code: {meta.code}</div> 
+			<div className='fine-print'><LatLng lat={props.currentLocation.lat} lng={props.currentLocation.lng} /></div>
+		</div>
+	)
+}
 
 export default class DetailCard extends Component {
-	
 	render() {
-		// if (!this.props.details) {
-		// 	return <div> Loading detailCard </div>
-		// } else {
 
-		// 	const properties = this.props.details.properties
-		// 	const meta = this.props.details.meta
-		// 	return (
-		// 		<div className={meta.code}>
-		// 			<h1>{this.props.location.city}, {this.props.location.state}</h1>
-		// 			<h2><Temperature temperature={properties.temperature} /></h2>
-		// 			<h3><Wind wind={properties.wind} /></h3>
-		// 			<div className='fine-print'>Sensor location: {meta.name}</div>
-		// 			<div className='fine-print'>Observation code: {meta.code}</div> 
-		// 			<div className='fine-print'><LatLng lat={this.props.location.lat} lng={this.props.location.lng} /></div>
-		// 		</div>
-		// 	)
-		// }
+		return (this.props.currentConditions.loadingData) ? <RetrievingData message='daily detail' /> : card(this.props)
 
-		return <div> hello from detail </div>
 	}
 }
