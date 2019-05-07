63320fc465e491a010e9d6db0cbe22c447e7496b
Brad Schrag <brad.schrag@gmail.com>
2019-05-07 13:22:52 -0500

    styling

--- client/src/components/stateless/myWeatherNavBar.js
+++ client/src/components/stateless/myWeatherNavBar.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import { Nav, Navbar } from 'react-bootstrap'
+import { Nav, Navbar, Col, Container } from 'react-bootstrap'
 import { withRouter } from 'react-router-dom'
 
 class MyWeatherNavBar extends Component {
@@ -11,19 +11,21 @@ class MyWeatherNavBar extends Component {
 
 	render() {
 
-		const setStyling = text => (this.props.match.params.displayType === text) ? {backgroundColor: 'white', color: 'black', width: 100} : {backgroundColor: 'transparent', color: 'white', width: 100}
+		const setStyling = text => (this.props.match.params.displayType === text) ? {backgroundColor: 'white', color: 'black'} : {backgroundColor: 'transparent', color: 'white'}
 
 		return (
+			<Container className="header">
 			<Navbar style={{padding: 0, backgroundColor: '#333333'}} expand='lg' variant='dark' >
 				<Navbar.Toggle aria-controls='basic-navbar-nav' />
 				<Navbar.Collapse id='basic-navbar-nav'>
-					<Nav className="my-weather">
-						<Nav.Link style={ setStyling('detail') } href={'/' + this.props.match.params.zip + '/detail'} onClick={ e => this.handleClick(e)} value='detail'>Detail</Nav.Link>
-						<Nav.Link style={ setStyling('hourly') } href={'/' + this.props.match.params.zip + '/hourly'} onClick={ e => this.handleClick(e)} value='hourly'>Hourly</Nav.Link>
-						<Nav.Link style={ setStyling('weekly') } href={'/' + this.props.match.params.zip + '/weekly'} onClick={ e => this.handleClick(e)} value='weekly'>Weekly</Nav.Link>
-					</Nav>
+						<Nav className="my-weather">
+							<Col xs='auto'><Nav.Link style={ setStyling('detail') } href={'/' + this.props.match.params.zip + '/detail'} onClick={ e => this.handleClick(e)} value='detail'>Detail</Nav.Link></Col>
+							<Col xs='auto'><Nav.Link style={ setStyling('hourly') } href={'/' + this.props.match.params.zip + '/hourly'} onClick={ e => this.handleClick(e)} value='hourly'>Hourly</Nav.Link></Col>
+							<Col xs='auto'><Nav.Link style={ setStyling('weekly') } href={'/' + this.props.match.params.zip + '/weekly'} onClick={ e => this.handleClick(e)} value='weekly'>Weekly</Nav.Link></Col>
+						</Nav>
 				</Navbar.Collapse>
 			</Navbar>
+			</Container>
 		)
 	}
 }
