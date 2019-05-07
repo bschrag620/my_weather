e1b82e7c1c971c7a1800f78b08e9420f31834ae8
Brad Schrag <brad.schrag@gmail.com>
2019-05-07 13:35:23 -0500

    added switch/case for units

--- client/src/reducers/session_reducer.js
+++ client/src/reducers/session_reducer.js
@@ -1,10 +1,22 @@
 function sessionReducer(state = {
-	currentLocation: null
+	currentLocation: null,
+	units: 'si'
 }, action) {
 	switch (action.type) {
+		let newUnit;
+
 		case 'SET_ACTIVE_LOCATION':
 			console.log('session reducer setting active location: ', action.location.id)
 			return {...state, currentLocation: action.location}
+
+		case 'TOGGLE_UNITS':
+			newUnit = (state.units === 'si') ? 'us' : 'si'
+			debugger;
+			return {...state, units: newUnit} 
+
+		case 'SET_UNITS':
+			debugger;
+			return {...state, ...action.payload}
 
 		default:
 			return state
