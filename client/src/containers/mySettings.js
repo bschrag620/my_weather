735dee8b51ce02a0fee5e1d2a3c55380152f3bf3
Brad Schrag <brad.schrag@gmail.com>
2019-05-07 13:30:10 -0500

    created settings component

---
+++ client/src/containers/mySettings.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class MySettings extends Component {
+
+	render() {
+		return (
+			<div>This is the settings component</div>
+		)
+
+	}
+}
+
+export default MySettings
