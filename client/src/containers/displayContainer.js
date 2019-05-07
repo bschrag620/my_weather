4ab02aead758c0c8a384f8f2b48db5488fe5192c
Brad Schrag <brad.schrag@gmail.com>
2019-05-07 00:51:10 -0500

    added weekly route

--- client/src/containers/displayContainer.js
+++ client/src/containers/displayContainer.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import RetreivingData from '../components/stateless/retrievingData'
-import DetailCard from '../components/detailCard'
-import HourlyCard from '../components/hourlyCard'
-import WeeklyCard from '../components/weeklyCard'
+import DetailDisplay from '../components/detailDisplay'
+import HourlyDisplay from '../components/hourlyDisplay'
+import WeeklyDisplay from '../components/weeklyDisplay'
 import { Container } from 'react-bootstrap'
 import { withRouter } from 'react-router-dom'
 
@@ -16,15 +16,18 @@ class DisplayContainer extends Component {
 
 		switch (this.props.match.params.displayType) {
 			case 'detail':
-				return <DetailCard currentLocation={this.props.currentLocation} currentConditions={this.props.allWeather.current} meta={this.props.allWeather.meta}/>
+				return <DetailDisplay currentLocation={this.props.currentLocation} currentConditions={this.props.allWeather.current} meta={this.props.allWeather.meta}/>
 
 			case 'hourly':
 				if (this.props.allWeather.hourly) {
-					return <HourlyCard currentLocation={this.props.currentLocation} hourlyForecast={this.props.allWeather.hourly} meta={this.props.allWeather.meta}/>
+					return <HourlyDisplay currentLocation={this.props.currentLocation} hourlyForecast={this.props.allWeather.hourly} meta={this.props.allWeather.meta}/>
 				}	
 
 			case 'weekly':
-				return <WeeklyCard />
+				if (this.props.allWeather.weekly) {
+					return <WeeklyDisplay currentLocation={this.props.currentLocation} weeklyForecast={this.props.allWeather.weekly} meta={this.props.allWeather.meta}/>	
+				}
+				
 
 			default:
 				return <div>Unrecognized type</div>
