ab0f36617aab0fd6f3e2c1032ace1f8b7ad87f28
Brad Schrag <brad.schrag@gmail.com>
2019-05-06 22:10:08 -0500

    building hourly display

--- client/src/components/hourlyCard.js
+++ client/src/components/hourlyCard.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
 import { CityState, LatLng, Temperature, Wind, Zip } from '../components/stateless/weatherIndex'
+import RetrievingData from './stateless/retrievingData'
+
+const generateHourlyCards = forecasts => {
+	debugger;
+}
 
 export default class HourlyCard extends Component {
 	
 	render() {
-
-		return <div> hello from hourly </div>
+		debugger;
+		return <div> {this.props.hourlyForecast.loadingData ? <RetrievingData message="hourly data" /> : generateHourlyCards(this.props.hourlyForecast) } </div>
 	}
 }
