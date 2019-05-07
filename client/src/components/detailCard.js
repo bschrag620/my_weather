58a801d676c26d9b976f1f6c6eda2e9985573fde
Brad Schrag <brad.schrag@gmail.com>
2019-05-06 23:19:28 -0500

    styling

--- client/src/components/detailCard.js
+++ client/src/components/detailCard.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react'
 import { CityState, LatLng, Temperature, Wind, Zip } from '../components/stateless/weatherIndex'
 import RetrievingData from './stateless/retrievingData'
+import { Container } from 'react-bootstrap'
 
 const card = props => {
 	const properties = props.currentConditions
 	const meta = props.meta
 	return (
-		<div className={meta.code}>
+		<Container className={meta.code}>
 			<h1>{props.currentLocation.city}, {props.currentLocation.state}</h1>
 			<h2><Temperature temperature={properties.temperature} /></h2>
 			<h3><Wind wind={properties.wind} /></h3>
 			<div className='fine-print'>Sensor location: {meta.name}</div>
 			<div className='fine-print'>Observation code: {meta.code}</div> 
 			<div className='fine-print'><LatLng lat={props.currentLocation.lat} lng={props.currentLocation.lng} /></div>
-		</div>
+		</Container>
 	)
 }
 
--- client/src/components/hourlyCard.js
+++ client/src/components/hourlyCard.js
@@ -1,15 +1,42 @@
-import React, { Component } from 'react'
-import { CityState, LatLng, Temperature, Wind, Zip } from '../components/stateless/weatherIndex'
+import React, { Component, Fragment } from 'react'
+import { CityState, LatLng, Temperature, Wind, Zip, Time } from '../components/stateless/weatherIndex'
 import RetrievingData from './stateless/retrievingData'
+import { Row, Col, Container } from 'react-bootstrap'
+import uuid from 'uuid'
 
 const generateHourlyCards = forecasts => {
-	debugger;
+	const dates = [...new Set(forecasts.map( f => f.date ))]
+	
+	//take this out!!!
+	console.log(forecasts[0])
+	return dates.map( date => {
+		const hourlyForecasts = forecasts.filter( f => f.date === date )
+
+		const hourlyCard = (props) => {
+			return (
+				<Col xs md lg='2' key={uuid()} style={{borderColor: '#333333', borderStyle: 'solid', borderWidth: '2px'}}>
+					<h4><Time time={props.startTime} /></h4>
+					<h2><Temperature temperature={props.temperature} /></h2>
+					<div className="wind"><Wind wind={props.wind} /></div>
+					<div className="shortDescription">{props.shortForecast}</div>
+				</Col>
+			)
+		}
+
+		return (
+			<Container key={uuid()} style={{borderStyle: 'solid', borderWidth: '1px', borderColor: '#333333'}}>
+				<Row style={{backgroundColor: '#333333', color: 'white', paddingLeft: '5px'}}><h1>{date}</h1></Row>
+				<Row>
+					{hourlyForecasts.map( (f, i) => hourlyCard(f, i))}
+				</Row>
+			</Container>
+		)
+	})
 }
 
 export default class HourlyCard extends Component {
 	
 	render() {
-		debugger;
-		return <div> {this.props.hourlyForecast.loadingData ? <RetrievingData message="hourly data" /> : generateHourlyCards(this.props.hourlyForecast) } </div>
+		return <Fragment> {this.props.hourlyForecast.loadingData ? <RetrievingData message="hourly data" /> : generateHourlyCards(this.props.hourlyForecast) } </Fragment>
 	}
 }
