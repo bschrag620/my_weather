b579777bf0f6afe42099b5f825b64c889d95c073
Brad Schrag <brad.schrag@gmail.com>
2019-05-08 10:01:43 -0500

    created

---
+++ client/src/components/stateless/settingsCardWithToggle.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import { Col, Row, Container } from 'react-bootstrap'
+import ToggleSwitch from '../toggleSwitch'
+
+class SettingsCardWithToggle extends Component {
+
+	render() {
+		return (
+			<Container className='card'>
+				<Col className='text-center'><h2>{this.props.title}</h2></Col>
+				<Row>
+					<Col className='text-center' size='auto'>
+						<h3>{this.props.leftLabel}</h3>
+					</Col>
+					<ToggleSwitch handleChange={this.props.handleChange} bool={this.props.bool} />
+					<Col className='text-center' size='auto'>
+						<h3>{this.props.rightLabel}</h3>
+					</Col>
+				</Row>
+			</Container>
+		)
+	}
+}
+
+export default SettingsCardWithToggle
