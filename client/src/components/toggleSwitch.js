cd3c40be851e63532c94211822a390ba66a43826
Brad Schrag <brad.schrag@gmail.com>
2019-05-08 08:54:56 -0500

    created toggleSwitch

---
+++ client/src/components/toggleSwitch.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import Switch from 'react-switch';
+
+class ToggleSwitch extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(checked) {
+  	this.props.handleChange()
+  }
+	render() {
+		
+		return (
+			<label htmlFor="toggle-switch">
+				<Switch
+					onChange={this.handleChange}
+					checked={this.props.bool}
+					id='toggle-switch'
+					checkedIcon={false}
+					uncheckedIcon={false}
+					offColor='#333'
+					onColor='#333'
+				/>
+			</label>
+		)
+	}
+}
+
+export default ToggleSwitch
