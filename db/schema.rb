1c4b2c0346d71f59177f0841b7e26830a7a400a4
Brad Schrag <brad.schrag@gmail.com>
2019-05-15 06:33:53 -0500

    migrated to pg db

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_04_25_201934) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "locations", force: :cascade do |t|
     t.float "lat"
     t.float "lng"
