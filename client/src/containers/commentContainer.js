166b21dead22cf084a5703d046813de482009cc5
Brad Schrag <brad.schrag@gmail.com>
2019-05-23 12:33:20 -0500

    updated

--- client/src/containers/commentContainer.js
+++ client/src/containers/commentContainer.js
@@ -10,22 +10,39 @@ export default class CommentContainer extends Component {
 	}
 
 	state = {
-		comments: []
+		comments: [{
+			text: 'hello', 
+			vote: 0},
+			{text: 'world',
+			vote: 0}]
 	}
 
 	logger = console.log('comment container loading...')
 
-	addComment = (text) => {
+	addComment(text) {
 		this.setState({
-			comments: this.state.comments.concat(text)
+			comments: this.state.comments.concat({text:text, vote: 0})
+		})
+	}
+
+	upVote(e) {
+		const id = parseInt(e.currentTarget.id)
+		const votedComment = this.state.comments[id]
+		votedComment.vote++
+		this.setState({
+			comments: 
+				[...this.state.comments.slice(0, id), 
+				votedComment, 
+				...this.state.comments.slice(id + 1)]
 		})
 	}
 		
 	render() {
 		return (
 			<div className="comment-container">
-				<CommentForm onSubmit={this.addComment}/>
-				<CommentsContainer comments={this.state.comments}/>
+				<CommentForm onSubmit={this.addComment.bind(this)}/>
+				
+				<CommentsContainer comments={this.state.comments} onUpVote={this.upVote.bind(this)} />
 			</div>
 		)
 	}
--- client/src/containers/commentsContainer.js
+++ client/src/containers/commentsContainer.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class CommentsContainer extends Component {
 	
 	renderComments = comments => {
-		return comments.map( (comment, i) => <div key={i} >{comment}</div>)	
+		return comments.map( (comment, i) => <div key={i} >{comment.text} {comment.vote} <button id={i} onClick={e => this.props.onUpVote(e) }>--upvote--</button></div>)	
 	}
 
 	render() {
